bc4dc26d7af6e9339097f24353abcf0b37129a94
Khoa Le <khoanguyenle2007@gmail.com>
2025-10-06 18:24:34 -0500

    Navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,60 @@
-import Image from "next/image";
+"use client";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>
-        ye
-      </h1>
+    <main className="p-8">
+
+      {/* Simple Shadcn Navigation Menu */}
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Menu</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[200px] gap-2 p-4">
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="#" className="block text-sm">
+                      Home
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="#" className="block text-sm">
+                      About
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink asChild>
+                    <Link href="#" className="block text-sm">
+                      Contact
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+
+          <NavigationMenuItem>
+            <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+              <Link href="#">Docs</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </main>
   );
 }
